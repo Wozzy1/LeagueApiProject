c1761b4070141274afa6e795b751439f09020cab
Wozzy1 <134657893+Wozzy1@users.noreply.github.com>
2023-12-30 09:54:40 -0600

    adjusted the constructor

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ api_key = "RGAPI-08576416-f07a-44ac-8e9c-777919706474"
 
 class GameDataGenerator():
 
-    # constructor with a player
     def __init__(self, _api_key, name) -> None:
-        self.Watcher = LolWatcher(_api_key)
-        self.Player = PlayerInfo("na1", name)
-
-    # constructor without a player
-    def __init__(self, _api_key) -> None:
-        self.Watcher = LolWatcher(_api_key)
-
+        if name is None:
+            self.Watcher = LolWatcher(_api_key)
+        elif name is not None:
+            self.Watcher = LolWatcher(_api_key)
+            self.Player = PlayerInfo("na1", name)
+        else:
+            print("An error occurred with the constructor for GameDataGenerator")
 
     def getPlayerMatches(self) -> list:        
         # returns a player object to be used in querying matches
@@ -43,14 +42,14 @@ class GameDataGenerator():
         participants = []
         while playernum < 10:
             for item in match_detail['info']['participants']:
-                part_row = {}
-                part_row['champion'] = match_detail['info']['participants'][playernum]['championName']
-                part_row['goldEarned'] = match_detail['info']['participants'][playernum]['goldEarned']
-                part_row['kills'] = match_detail['info']['participants'][playernum]['kills']
-                part_row['deaths'] = match_detail['info']['participants'][playernum]['deaths']
-                part_row['assists'] = match_detail['info']['participants'][playernum]['assists']
+                participant_row = {}
+                participant_row['champion'] = match_detail['info']['participants'][playernum]['championName']
+                participant_row['goldEarned'] = match_detail['info']['participants'][playernum]['goldEarned']
+                participant_row['kills'] = match_detail['info']['participants'][playernum]['kills']
+                participant_row['deaths'] = match_detail['info']['participants'][playernum]['deaths']
+                participant_row['assists'] = match_detail['info']['participants'][playernum]['assists']
                 #print(match_detail['info']['participants'][playernum]['championId'])
-                participants.append(part_row)
+                participants.append(participant_row)
                 playernum +=1
         #print(match_detail['info']['participants'][0])
         df = pd.DataFrame(participants)
@@ -64,21 +63,21 @@ class GameDataGenerator():
             match_detail = self.Watcher.match.by_id(self.Player.region, last_match) # doesnt need error catching becasue is done in a controlled for loop
             
             # simplify to for in range 10?
-            for part_num in range(len(match_detail['info']['participants'])):
-                #print(match_detail['info']['participants'][part_num]['summonerName'])
-                if match_detail['info']['participants'][part_num]['summonerName'] == self.Player.name:
-                    part_row = {}
-                    part_row['Win'] = match_detail['info']['participants'][part_num]['win']
-                    part_row['championPlayed'] = match_detail['info']['participants'][part_num]['championName']
-                    # part_row['championId'] = match_detail['info']['participants'][part_num]['championId']
-                    part_row['gameLength (sec)'] = match_detail['info']['participants'][part_num]['timePlayed']
-                    part_row['kills'] = match_detail['info']['participants'][part_num]['kills']
-                    part_row['deaths'] = match_detail['info']['participants'][part_num]['deaths']
-                    part_row['assists'] = match_detail['info']['participants'][part_num]['assists']
-                    part_row['goldEarned'] = match_detail['info']['participants'][part_num]['goldEarned']
-                    # part_row['CS'] = match_detail['info']['participants'][part_num]['totalMinionsKilled'] # FIX THE COMPUTATION OF TOTAL CS
-                    part_row['dmgToChamps'] = match_detail['info']['participants'][part_num]['totalDamageDealtToChampions']
-                    games_info.append(part_row)
+            for participant_num in range(len(match_detail['info']['participants'])):
+                #print(match_detail['info']['participants'][participant_num]['summonerName'])
+                if match_detail['info']['participants'][participant_num]['summonerName'] == self.Player.name:
+                    participant_row = {}
+                    participant_row['Win'] = match_detail['info']['participants'][participant_num]['win']
+                    participant_row['championPlayed'] = match_detail['info']['participants'][participant_num]['championName']
+                    # participant_row['championId'] = match_detail['info']['participants'][participant_num]['championId']
+                    participant_row['gameLength (sec)'] = match_detail['info']['participants'][participant_num]['timePlayed']
+                    participant_row['kills'] = match_detail['info']['participants'][participant_num]['kills']
+                    participant_row['deaths'] = match_detail['info']['participants'][participant_num]['deaths']
+                    participant_row['assists'] = match_detail['info']['participants'][participant_num]['assists']
+                    participant_row['goldEarned'] = match_detail['info']['participants'][participant_num]['goldEarned']
+                    # participant_row['CS'] = match_detail['info']['participants'][participant_num]['totalMinionsKilled'] # FIX THE COMPUTATION OF TOTAL CS
+                    participant_row['dmgToChamps'] = match_detail['info']['participants'][participant_num]['totalDamageDealtToChampions']
+                    games_info.append(participant_row)
         df = pd.DataFrame(games_info)
         # return(print(df))
         return df
@@ -86,26 +85,44 @@ class GameDataGenerator():
     """
     For this method, I want to get data in a different fashion. I want to look at the game in a broader sense rather 
     than focusing on one player. Some conditions that are strongly associated with winning are:
-    - gold difference
-    - avg champion levels
-    - total exp
-    - minions killed
-    - jungle monsters killed
-    - dragons killed
-        - elder dragon
-    - rift heralds taken
-    - barons
-    - towers destroyed
-    
+    - avg champion levels (float)
+    - total exp (int)
+    - minions killed (int)
+    - jungle monsters killed (int)
+    - avg gold difference (float)
+    - lane gold difference
+        - top vs top
+        - jg vs jg
+        - mid vs mid
+        - bot vs bot
+        - sup vs sup
+    - dragons killed (int)
+        - elder dragon (int)
+    - rift heralds taken (int)
+    - barons (int)
+    - towers destroyed (int)
+    - blue team win (boolean)
+    - team total KDA
+        - Kills
+        - Deaths
+        - Assists
+    - vision score
+    - pink wards
+    - wards placed
+    - wards killed
     """
     def getMultipleGameStatsById(self, matchIdList):
         games_info = []
         for matchId in matchIdList:
             match_detail = self.Watcher.match.by_id("na1", matchId)
-
+            game_info = {}
+            game_info['gameId'] = matchId
+            game_info['blueTeamWin'] = match_detail['info']['teams'][0]['win']
+            games_info.append(game_info)
+        df = pd.DataFrame(games_info)
             
 
-        return
+        return df
 
     
 class PlayerInfo():
